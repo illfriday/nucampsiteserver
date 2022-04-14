4ef4f9f188b2d8f3d0c7fd7bdac2e33b7c56f015
illfriday <willprouty@gmail.com>
2022-04-14 14:51:32 -0700

    use express-session and session-file-stroe middleware to store cookies/session data instead of cookie-parser

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require("path");
 //import 'cookie-parser' MIDDLEWARE to parse COOKIES / process SIGNED COOKIES
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+//import EXPRESS-SESSION MIDDLEWARE to store user/session data on SERVER-SIDE
+const session = require("express-session");
+// ES6 1st CLASS FUNCTIONS allow us to CALL the REQUIRE FUNCTION from 'session' as a 2nd PARAMETER??
+const fileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -39,15 +43,26 @@ app.set("view engine", "pug");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("5454548-54545-87891215a@rT5&8"));
+// app.use(cookieParser("5454548-54545-87891215a@rT5&8"));
 //use 'cookie-parser' MIDDLEWARE to process SIGNED COOKIES. passing a random KEY/SIGNATURE to encrypt the information
 
+//set up 'express-session' MIDDLEWARE with a name, secret key, configs & use 'session-file-store' MIDDLEWARE to store the SESSION INFO in the SERVER'S storage
+app.use(
+  session({
+    name: "session-id",
+    secret: "5454548-54545-87891215a@rT5&8",
+    saveUninitialized: false,
+    resave: false,
+    store: new fileStore(),
+  })
+);
 //Implement CUSTOM AUTHENTICATION MIDDLEWARE vvv before we begin to route the REQUEST, or serve STATIC HTML PAGES. MIDDLEWARE runs in sequence parsing the request, adding to or TERMINATING the RESPONSE
 function auth(req, res, next) {
-  // console.log(req.headers);
+  console.log(req.session);
 
   //.signedCookie PROPERTY of the REQUEST OBJECT(.req). if COOKIE is not properly SIGNED, returns VALUE of FALSE. The .user PROPERTY is added by us(CUSTOM)
-  if (!req.signedCookies.user) {
+  // if (!req.signedCookies.user) {
+  if (!req.session.user) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       const err = new Error("You are not authenticated!");
@@ -62,8 +77,9 @@ function auth(req, res, next) {
     const user = auth[0];
     const pass = auth[1];
     if (user === "admin" && pass === "password") {
-      res.cookie("user", "admin", { signed: true });
+      // res.cookie("user", "admin", { signed: true });
       //res.cookie() uses EXPRESS's .res OBJECT's API to create a SIGNED COOKIE OBJECT with the name we will use('user'): COOKIE : {'user':'admin', signed:true} tells EXPRESS to use the SECRET KEY from 'cookie-parser' MIDDLEWARE^^^^
+      req.session.user = "admin";
       return next(); // authorized
     } else {
       const err = new Error("You are not authenticated!");
@@ -72,7 +88,8 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if (req.signedCookies.user === "admin") {
+    //
+    if (req.session.user === "admin") {
       return next();
     } else {
       const err = new Error("You are not authenticated.");
